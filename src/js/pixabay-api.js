74b2b753f63718f3cc8f752952104785a5745b11
Denys Koval <metalheadden@gmail.com>
2025-05-17 14:06:31 +0300

    hw11-01

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+
+export function getImagesByQuery(query) {
+  const myApiKey = "39362168-c2a5e7696671cec7e0c5dcb17";
+  const BASE_URL = "https://pixabay.com/api/";
+  const params = new URLSearchParams({
+    key: myApiKey,
+    q: query,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+  })
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: myApiKey,
+        q: query,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+      }
+    })
+    .then(response => response.data) 
+    .catch(error => error.message)
+};
+
+
